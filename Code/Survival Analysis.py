95161dd211b755dc137e8418b1642a01b75a2b7f
Joy M. Nyaga <joyaga95@gmail.com>
2018-11-15 00:29:32 +0300

    Update Survival Analysis.py

--- Code/Survival Analysis.py
+++ Code/Survival Analysis.py
@@ -8,13 +8,11 @@ Created on Mon Nov  5 18:01:00 2018
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
 from lifelines import KaplanMeierFitter
 
 #Load data
 url="https://archive.ics.uci.edu/ml/machine-learning-databases/haberman/haberman.data"
 data=pd.read_csv(url,header=None)
-
 
 #Inspect data
 data.head()
